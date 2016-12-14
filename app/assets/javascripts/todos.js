80952cc8849966c59ca7d27803fe4b24d43da865
近藤豊峰 <atsumine@atsumine.local>
2016-12-14 18:55:18 +0900

    とりあえずvue-resourceでデータを取得してこれるように

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+
+$(document).on('ready page:load', function() {
+        new Vue({
+            el: "#todo_index",
+            data: {
+                items: []
+            },
+            created: function() {
+                console.log('hoge');
+                this.$http.get('/todos.json').then(function(response){
+                    // success callback
+                    console.log(response);
+                }, function(response) {
+                    console.log(response);
+                    // error callback
+                });
+            }
+        });
+});
