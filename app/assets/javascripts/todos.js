d5f80aa79e2f9bb6ef5cb03bd12e712f4e08eb95
近藤豊峰 <atsumine@atsumine.local>
2016-12-15 13:56:42 +0900

    add migrate files

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -8,11 +8,10 @@ $(document).on('ready page:load', function() {
             created: function() {
                 this.$http.get('/todos.json').then(function(response, status, request){
                     for(var i = 0; i < response.body.length; i++){
-                        console.log(response.body[i]);
                         this.todos.push(response.body[i]);
                     }
                 }, function(response) {
-                    console.error('request failed.');
+                    console.error('GET /todos.json failed. Check log file.');
                 });
             }
         });
