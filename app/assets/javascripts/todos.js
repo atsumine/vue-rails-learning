82fd54138b14434c6b81bf119fd5fa0cb80d2c55
近藤豊峰 <atsumine@atsumine.local>
2016-12-19 14:17:15 +0900

    add deleteTodo

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,4 +1,15 @@
 
+
+function twoDigits(d) {
+    if(0 <= d && d < 10) return "0" + d.toString();
+    if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+    return d.toString();
+}
+
+Date.prototype.toMysqlFormat = function() {
+    return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
+};
+
 $(document).on('ready page:load', function() {
         var app = new Vue({
             el: "#todo_index",
@@ -6,7 +17,7 @@ $(document).on('ready page:load', function() {
                 todos: []
             },
             created: function() {
-                this.$http.get('/todos.json').then(function(response, status, request){
+                this.$http.get('/todos.json').then(function(response){
                     for(var i = 0; i < response.body.length; i++){
                         this.todos.push(response.body[i]);
                     }
@@ -21,10 +32,16 @@ $(document).on('ready page:load', function() {
                         console.log('empty input.');
                         return;
                     }
-                    this.todos.push({name: this.input, deadline: Date.now(), done: false});
+                    this.todos.push({name: this.input, deadline: Date.now().toMysqlFormat, done: false});
+                    console.log(Date.now().toMysqlFormat());
                     this.input = '';
                     console.log('todo added successfully!!');
-                    return;
+                },
+
+                deleteTodo: function (index) {
+                    console.log(index);
+                    this.todos.splice(index, 1);
+                    console.log('deleteTodo done.');
                 }
             }
         });
