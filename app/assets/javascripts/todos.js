6d6f2bed44ba35706d088db553faf1c8dce90ff2
近藤豊峰 <atsumine@atsumine.local>
2016-12-19 14:20:22 +0900

    remove duplicate statements

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,18 +1,6 @@
-
-
-function twoDigits(d) {
-    if(0 <= d && d < 10) return "0" + d.toString();
-    if(-10 < d && d < 0) return "-0" + (-1*d).toString();
-    return d.toString();
-}
-
-Date.prototype.toMysqlFormat = function() {
-    return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
-};
-
 $(document).on('ready page:load', function() {
-        var app = new Vue({
-            el: "#todo_index",
+        var todoApp = new Vue({
+            el: "#todoIndex",
             data: {
                 todos: []
             },
@@ -27,21 +15,13 @@ $(document).on('ready page:load', function() {
             },
             methods: {
                 addTodo: function (event){
-                    console.log('addTodo successfully called!!');
-                    if (this.input === '') {
-                        console.log('empty input.');
-                        return;
-                    }
-                    this.todos.push({name: this.input, deadline: Date.now().toMysqlFormat, done: false});
-                    console.log(Date.now().toMysqlFormat());
+                    if (this.input === '') return;
+                    this.todos.push({name: this.input, deadline: Date.now(), done: false});
                     this.input = '';
-                    console.log('todo added successfully!!');
                 },
 
                 deleteTodo: function (index) {
-                    console.log(index);
                     this.todos.splice(index, 1);
-                    console.log('deleteTodo done.');
                 }
             }
         });
